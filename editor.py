afcd1880daeb4b40538f09b219bbdf989f5491af
MRSDN <business@kingbnjmn.com>
2025-09-19 12:33:09 -0500

    added level editor, cleaned up a lil

--- editor.py
+++ editor.py
@@ -33,16 +33,42 @@ class Editor:
         self.tile_variant = 0
         self.clicking = False
         self.right_clicking = False
+        self.shift = False
 
     def run(self):
 
+        # rendering the window
+
         while True:
             self.display.fill((0, 0, 0))
-
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+            self.tilemap.render(self.display, offset=render_scroll)
             current_tile_img = self.assets[self.tile_list[self.tile_group]][self.tile_variant].copy()
             current_tile_img.set_alpha(100)
 
+            # mouse cursor input stuff
+
+            mpos = pygame.mouse.get_pos()
+            mpos = (mpos[0] / RENDER_SCALE, mpos[1] / RENDER_SCALE)
+            tile_pos = (int((mpos[0] + self.scroll[0]) // self.tilemap.tile_size),
+                        int((mpos[1] + self.scroll[1]) // self.tilemap.tile_size))
+
+            # place a tile
+
+            if self.clicking:
+                self.tilemap.tilemap[str(tile_pos[0]) + ';' + str(tile_pos[1])] = \
+                    {'type': self.tile_list[self.tile_group], 'variant': self.tile_variant, 'pos': tile_pos}
+
+            # delete a tile
+
+            if self.right_clicking:
+                tile_loc = str(tile_pos[0]) + ';' + str(tile_pos[1])
+                if tile_loc in self.tilemap.tilemap:
+                    del self.tilemap.tilemap[tile_loc]
+
             self.display.blit(current_tile_img, (5, 5))
+
+            # KB input & mouse clicking/scrolling
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -54,10 +80,24 @@ class Editor:
                         self.clicking = True
                     if event.button == 3:
                         self.right_clicking = True
-                    if event.button == 4:
-                        self.tile_group = (self.tile_group - 1) % len(self.tile_list)
-                    if event.button == 5:
-                        self.tile_group = (self.tile_group + 1) % len(self.tile_list)
+                    if self.shift:
+                        if event.button == 4:
+                            self.tile_variant = (self.tile_variant - 1) % len(self.assets[self.tile_list[self.tile_group]])
+                        if event.button == 5:
+                            self.tile_variant = (self.tile_variant + 1) % len(self.assets[self.tile_list[self.tile_group]])
+                    else:
+                        if event.button == 4:
+                            self.tile_group = (self.tile_group - 1) % len(self.tile_list)
+                            self.tile_variant = 0
+                        if event.button == 5:
+                            self.tile_group = (self.tile_group + 1) % len(self.tile_list)
+                            self.tile_variant = 0
+
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if event.button == 1:
+                        self.clicking = False
+                    if event.button == 3:
+                        self.right_clicking = False
 
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
@@ -68,6 +108,8 @@ class Editor:
                         self.movement[2] = True
                     if event.key == pygame.K_DOWN:
                         self.movement[3] = True
+                    if event.key == pygame.K_LSHIFT:
+                        self.shift = True
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
@@ -77,6 +119,10 @@ class Editor:
                         self.movement[2] = False
                     if event.key == pygame.K_DOWN:
                         self.movement[3] = False
+                    if event.key == pygame.K_LSHIFT:
+                        self.shift = False
+
+            # scaling pixels here
 
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
